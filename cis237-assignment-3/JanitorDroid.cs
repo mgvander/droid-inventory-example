7cfc52923388e18878c16ae7a18ce27caf982ac8
David Barnes <dbarnes@kvcc.edu>
2021-07-23 18:59:51 -0400

    Complete Assignment 3.

---
+++ cis237-assignment-3/JanitorDroid.cs
@@ -0,0 +1,63 @@
+﻿// Author: David Barnes
+// Class: CIS 237
+// Assignment: 3
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cis237_assignment_3
+{
+    class JanitorDroid : UtilityDroid
+    {
+        // Some protected variables that can be accessed in derived classes
+        protected bool hasTrashCompactor;
+        protected bool hasVacuum;
+
+        // Constuctor that takes lots of parameters to create the droid. The base constructor is used to do some of the work
+        public JanitorDroid(string Material, string Color,
+            bool HasToolbox, bool HasComputerConnection, bool HasArm, bool HasTrashCompactor, bool HasVacuum) :
+            base(Material, Color, HasToolbox, HasComputerConnection, HasArm)
+        {
+            // Set the Droid Cost
+            MODEL_COST = 160.00m;
+            // Assign the values that the base constructor is not taking care of.
+            this.hasTrashCompactor = HasTrashCompactor;
+            this.hasVacuum = HasVacuum;
+        }
+
+        // Override the CalculateCostOfOptions method.
+        // Use the base class implementation of the method, and tack on the cost of the new options
+        protected override decimal CalculateCostOfOptions()
+        {
+            decimal optionsCost = 0;
+
+            optionsCost += base.CalculateCostOfOptions();
+
+            if (hasTrashCompactor)
+            {
+                optionsCost += COST_PER_OPTION;
+            }
+
+            if (hasVacuum)
+            {
+                optionsCost += COST_PER_OPTION;
+            }
+
+            return optionsCost;
+        }
+
+        protected override string GetModelName()
+        {
+            return "Janitor";
+        }
+
+        // Overridden ToString that uses the base ToString method, and appends the missing information.
+        public override string ToString()
+        {
+            return
+                base.ToString() +
+                "Has Trash Compactor: " + this.hasTrashCompactor + Environment.NewLine +
+                "Has Vacuum: " + this.hasVacuum + Environment.NewLine;
+        }
+    }
+}
--- cis237-assignment-3/Program.cs
+++ cis237-assignment-3/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Author: David Barnes
+// Class: CIS 237
+// Assignment: 3
+using System;
 
 namespace cis237_assignment_3
 {
@@ -6,7 +9,41 @@ namespace cis237_assignment_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Create a new droid collection and set the size of it to 100.
+            IDroidCollection droidCollection = new DroidCollection(100);
+
+            // Create a user interface and pass the droidCollection into it as a dependency
+            UserInterface userInterface = new UserInterface(droidCollection);
+
+            // Display the main greeting for the program
+            userInterface.DisplayGreeting();
+
+            // Display the main menu for the program
+            userInterface.DisplayMainMenu();
+
+            // Get the choice that the user makes
+            int choice = userInterface.GetMenuChoice();
+
+            // While the choice is not equal to 3, continue to do work with the program
+            while (choice != 3)
+            {
+                // Test which choice was made
+                switch (choice)
+                {
+                    // Choose to create a droid
+                    case 1:
+                        userInterface.CreateDroid();
+                        break;
+
+                    // Choose to Print the droid
+                    case 2:
+                        userInterface.PrintDroidList();
+                        break;
+                }
+                // Re-display the menu, and re-prompt for the choice
+                userInterface.DisplayMainMenu();
+                choice = userInterface.GetMenuChoice();
+            }
         }
     }
 }
